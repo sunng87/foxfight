648d56b433987076d1c076c88dda579f68f9affa
Sun Ning <nsun@avos.com>
2013-12-02 16:13:30 +0800

    updated jetty

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [info.sunng/ring-jetty9-adapter "0.2.0"]
+                 [info.sunng/ring-jetty9-adapter "0.3.0"]
                  [compojure "1.1.5"]
                  [hbs "0.4.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
